ef2587cbb2198644507f3d847c463addaa6af435
TyurinV <denfal486@gmailc.om>
2021-02-13 19:51:46 +0300

    Combined 2 steps

--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -1,17 +1,12 @@
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.io.File;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class GitHub {
 
@@ -24,18 +19,15 @@ public class GitHub {
     @Test
     void shouldHaveCodeInGithub() {
         //Открыть страницу Github
-    open("https://github.com/");
+        open("https://github.com/");
         // Открыть Selenide
-    $("[name=q]").setValue("selenide").pressEnter();
-    $$("ul.repo-list li a").get(0).click();
+        $("[name=q]").setValue("selenide").pressEnter();
+        $(".repo-list li a").click();
         // Перейти в Wiki
-    $$("ul.UnderlineNav-body li").get(4).$("a").click();
-        //Проверка что в списке страниц (Pages) есть страница SoftAssertions
-    $$("div.js-wiki-sidebar-toggle-display li a").shouldHave(CollectionCondition.itemWithText("SoftAssertions"));
-        // Откройте страницу SoftAssertions
-    $$("div.js-wiki-sidebar-toggle-display li a").findBy(text("SoftAssertions")).click();
+        $(byText("Wiki")).click();
+        //Проверка что в списке страниц (Pages) есть страница SoftAssertions + Откройте страницу SoftAssertions
+        $(byText("SoftAssertions")).shouldHave(text("SoftAssertions")).click();
         // Проверить что внутри есть пример кода для JUnit5
-    $("#wiki-body").shouldHave(Condition.text("Using JUnit5 extend test class"));
-        Selenide.sleep(6000);
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
